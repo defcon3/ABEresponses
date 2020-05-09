89937b19b906c2e4ce7f7b6aeb64a4ad892a058f
Veit Luther <defcon3@live.com>
2020-05-09 09:51:58 +0200

    Assemblyinformationen zugefuegt

---
+++ ABEresponses/My Project/AssemblyInfo.vb
@@ -0,0 +1,6 @@
+Imports System.Resources
+Imports System.Reflection
+
+<Assembly: AssemblyTitle("Antwortobjekte")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
+<Assembly: AssemblyVersion("1.0.0.1")>
